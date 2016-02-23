b6d5242290f49776ae14f39bea54a1b9a4d465ec
Jsteiginga <Justin_steiginga@hotmail.com>
2016-02-23 14:05:08 +0100

    Probleem 4 toegevoegd

---
+++ ProjectsEuler/src/ProjectEuler4.java
@@ -0,0 +1,15 @@
+public class ProjectEuler4 {
+
+
+    public static void main(String args[]){
+        int a;
+        int b;
+        int max = 99;
+        for (a = 0;a<=max;a++){
+            for (b=0;b<=max;b++){
+                System.out.println(a +" * " + b + " = " + a*b);
+            }
+        }
+
+            }
+}
